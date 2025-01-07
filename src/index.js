713c5fffd41896c8a73ae02e273ebe14096c35aa
Kyler Figgins <kyler.figgins@gmail.com>
2025-01-06 22:51:29 -0500

    Setup basic hash routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-addEventListener("hashchange", () => {
-  const hash = window.location.hash;
-  console.log(hash);
-});
+const renderPage = () => {
+  const app = document.getElementById("app");
+  const route = window.location.hash.slice(1);
+  console.log(app, route);
 
-window.location.hash = "#newHash";
+  switch (route) {
+    case "home":
+      app.innerHTML = `<h1>Home Page</h1>`;
+      break;
+    case "other":
+      app.innerHTML = `<h1>Other Page</h1>`;
+      break;
+    default:
+      app.innerHTML = `<h1>Choose page</h1>
+      <a href="#home">Home</a>
+      <a href="#other">Other</a>`;
+  }
+};
+
+addEventListener("hashchange", renderPage);
+addEventListener("load", renderPage);
