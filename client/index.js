ff44f453c2ba9ba3989b2e0ea89a1d1030ff94a3
Kyler Figgins <kyler.figgins@gmail.com>
2025-01-09 15:53:51 -0500

    Make first call to weather api

--- client/index.js
+++ client/index.js
@@ -1,16 +1,16 @@
 import localWeather from "./views/local-weather/index.js";
 import radar from "./views/radar/index.js";
 
-const renderPage = () => {
+const renderPage = async () => {
   const app = document.getElementById("app");
   const route = window.location.hash.slice(1);
 
   switch (route) {
     case "local":
-      app.innerHTML = localWeather();
+      app.innerHTML = await localWeather();
       break;
     case "radar":
-      app.innerHTML = radar();
+      app.innerHTML = await radar();
       break;
     default:
       app.innerHTML = `<h1>Welcome to the weather app</h1>
--- client/views/local-weather/index.js
+++ client/views/local-weather/index.js
@@ -1,3 +1,30 @@
-export default () => {
-  return `<h1>Local Weather</h1>`;
+export default async () => {
+  const weatherData = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=40.072720&longitude=-85.990140&current_weather=true",
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+
+  return `<div>
+           <h1>Local Weather</h1>
+
+           <p>Temperature: ${convertToFahrenheit(weatherData.current_weather.temperature)} F</p><p>Temperature: ${weatherData.current_weather.temperature} C</p>
+           <p>Wind Speed: ${weatherData.current_weather.windspeed}</p>
+           <p>Wind Direction: ${weatherData.current_weather.winddirection}</p>
+
+          </div>`;
+};
+
+const convertToFahrenheit = (celsius) => {
+  return (celsius * 9) / 5 + 32;
 };
